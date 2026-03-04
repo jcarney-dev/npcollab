3c20a14fb1fc6f64b27d77a201c3999a0c461fd2
jcarney-dev <130114280+jcarney-dev@users.noreply.github.com>
2026-03-04 11:05:26 +1100

    Fix all import paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // When deploying to a cPanel subfolder e.g. public_html/npcollab
-  // Change 'npcollab' to match your exact subfolder name
   base: '/npcollab',
   trailingSlash: 'always',
   build: {
     assets: 'assets'
-  },
-  vite: {
-    resolve: {
-      alias: {
-        '@layouts': '/src/layouts',
-        '@components': '/src/components',
-        '@content': '/src/content',
-      }
-    }
   }
 });
